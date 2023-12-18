7a3f8dab7e750d32b786625213769ad84fc7bf6f
veronikaromaniv <115730889+veronikaromaniv@users.noreply.github.com>
2023-12-18 16:34:58 +0200

    Update Payment_Subsystem.py

--- Lab_6/Payment_Subsystem.py
+++ Lab_6/Payment_Subsystem.py
@@ -1,57 +1,16 @@
-from shemas.Payment_Subsystem import *
-
-
-class ShoppingCart:
+class Payment:
     def __init__(self):
-        self.cart_items = {}
+        self.card_details = 0
 
-    def add_item(self, product_id, quantity):
-        self.cart_items[product_id] = quantity
+    def add_card_details(self, balance: int):
+        self.card_details = balance
 
-    def update_quantity(self, product_id, new_quantity):
-        if product_id in self.cart_items:
-            self.cart_items[product_id] = new_quantity
+    def verify_payment(self, cart_details: int, cart_items: dict, products: dict):
+        total_payment = 0
 
-    def checkout(self, stock_items: dict, cart_details: int, products: dict):
-        payment = Payment()
+        for product_id, quantity in cart_items.items():
+            if product_id in products:
+                product_price = products[product_id]
+                total_payment += quantity * product_price
 
-        payment.add_card_details(cart_details)
-        if payment.verify_payment(cart_details, self.cart_items, products) and self.check_stock_quantity(stock_items):
-            return True
-        else:
-            return False
-
-    def check_stock_quantity(self, stock_items: dict):
-        for product_id, cart_quantity in self.cart_items.items():
-            if product_id not in stock_items:
-                return False
-
-            stock_quantity = stock_items[product_id]
-
-            if cart_quantity > stock_quantity:
-                return False
-
-        return True
-
-class Order:
-    def __init__(self):
-        self.orders = {}
-        self.next_order_id = 1
-
-    def create_order(self, cart, customer_info):
-        order_id = self.next_order_id
-        self.next_order_id += 1
-        order_data = {
-            "cart": cart,
-            "customer_info": customer_info,
-
-        }
-        self.orders[order_id] = order_data
-        return order_id
-
-    def edit_order(self, order_id, new_info):
-        if order_id in self.orders:
-            self.orders[order_id].update(new_info)
-            return True
-        else:
-            return False
+        return cart_details >= total_payment
